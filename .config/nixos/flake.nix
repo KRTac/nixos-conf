6a883a139b2187b596f93505d713c2c304b45154
Dino Krtanjek <krtanjekdino@gmail.com>
2026-08-16 03:17:30 +0200

    Trying qit version of quickshell to get full functionality

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -3,6 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    quickshell = {
+      url = "git+https://git.outfoxxed.me/quickshell/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ self, nixpkgs, ... }: {
--- .config/nixos/modules/dmshell.nix
+++ .config/nixos/modules/dmshell.nix
@@ -23,6 +23,7 @@
 
   programs.dms-shell = {
     enable = true;
+    quickshell.package = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.quickshell;
 
     systemd = {
       enable = true;             # Systemd service for auto-start
