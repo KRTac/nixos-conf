f1588aca3b7a03d6dab9a75156b5d4d7ea68d221
Dino Krtanjek <krtanjekdino@gmail.com>
2026-08-21 19:17:53 +0200

    PRIME reverse sync enabled

--- modules/zenbook.nix
+++ modules/zenbook.nix
@@ -27,6 +27,8 @@
     modesetting.enable = true;
     nvidiaSettings = true;
     prime = {
+      offload.enable = false;
+      reverseSync.enable = true;
       intelBusId = "PCI:0@0:2:0";
       nvidiaBusId = "PCI:1@0:0:0";
     };
