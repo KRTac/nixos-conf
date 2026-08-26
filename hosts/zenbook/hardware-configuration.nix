b386290867f3331e422b8647579bec317b34cad8
Dino Krtanjek <krtanjekdino@gmail.com>
2026-08-26 16:38:16 +0200

    Removed unused inputs

--- hosts/zenbook/hardware-configuration.nix
+++ hosts/zenbook/hardware-configuration.nix
@@ -1,12 +1,10 @@
 { config, lib, pkgs, modulesPath, ... }: {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "sdhci_pci" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.kernelModules = [];
   boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+  boot.extraModulePackages = [];
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/63864627-a0fb-4568-8406-86144a62f186";
