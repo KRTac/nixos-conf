004e4f2bf7e1ff875206fcab11be6c0b557bef22
Dino Krtanjek <krtanjekdino@gmail.com>
2026-08-27 17:43:09 +0200

    Added disko as a module

--- flake.nix
+++ flake.nix
@@ -3,12 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }: {
+  outputs = inputs@{ self, nixpkgs, disko, ... }: {
     nixosConfigurations.zenbook = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       modules = [
+        disko.nixosModules.disko
+        ./hosts/zenbook/disko-config.nix
         ./hosts/zenbook/configuration.nix
       ];
     };
